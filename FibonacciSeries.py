73ca1c3b802a4076a1793395a2a75a013a4dbf20
Karnsinh96 <91832296+Karnsinh96@users.noreply.github.com>
2021-10-12 19:30:30 +0530

    Update FibonacciSeries.py

--- FibonacciSeries.py
+++ FibonacciSeries.py
@@ -1,5 +1,16 @@
-x=0
-y=1
-while y<50:
-	print(y)
-	x,y=y,x+y
+n = int(input("How Many terms of Fibonacci series you want"))
+
+x = 0
+y = 1
+z = 0
+
+if (n <= 0):
+    print("Enter Positive Number")
+elif (n == 1):
+    print("The Fibonacci series is ", x)
+else:
+    print("The Fibonacci series is ")
+    while (z <= n):
+        print(z)
+        x = y
+        y = z
